cfb72131ed09a82b1fc0afafc5c50fb1318b0c45
이소정 <sojeong952@naver.com>
2024-08-16 00:38:08 +0900

    chore: project init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    // 포트 번호 설정
+    port: 3000,
+    // 핫 모듈 교체(HMR) 활성화 설정
+    hot: true
+  }
+};
